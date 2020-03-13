66237d9da0d0aecb96ee94a8919d7dba2310954b
Komij <akpa125@gmail.com>
2020-03-14 00:34:29 +0330

    addd router

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import './menu-item.style.scss'
-const MenuItem = ({ title, imageUrl ,size }) => (
-    <div  className={ `${size} menu-item`}>
-        <div className='background-image'  style={{
-        backgroundImage:`url(${imageUrl})`
-    }}/>
+import './menu-item.style.scss';
+import { withRouter } from 'react-router-dom';
+
+const MenuItem = ({ title, imageUrl, size, linkUrl, match, history }) => (
+    <div onClick={() => history.push(`${match.url}${linkUrl}`)} className={`${size} menu-item`}>
+        <div className='background-image' style={{
+            backgroundImage: `url(${imageUrl})`
+        }} />
         <div className='content'>
             <div className='title'>{title.toUpperCase()}</div>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
 )
-export default MenuItem;
+export default withRouter(MenuItem);
